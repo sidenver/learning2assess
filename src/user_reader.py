28ce22f6d31d3a2a1bd6d34defe4de212f8b6b08
Han-Chin Shing <shing@umiacs.umd.edu>
2019-11-30 23:42:17 -0500

    add post-level model

--- src/user_reader.py
+++ src/user_reader.py
@@ -168,6 +168,64 @@ class UserCLPsychDatasetReader(DatasetReader):
                                             user["timestamp"], user["label"])
 
 
+@DatasetReader.register('post_clpsych_reader')
+class PostCLPsychDatasetReader(DatasetReader):
+    """
+    For pre-sentenized, pre-tokenized json-line SuicideWatch Dataset
+    """
+
+    def __init__(self,
+                 token_indexers: Optional[Dict[str, TokenIndexer]] = None,
+                 max_doc: int = 50,
+                 max_sent: int = 16,
+                 max_word: int = 64,
+                 lazy: bool = False) -> None:
+        super().__init__(lazy=lazy)
+        self.max_doc = max_doc
+        self.max_sent = max_sent
+        self.max_word = max_word
+        self.token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+
+    def tokens_to_user_field(self, tokens) -> ListField:
+        doc_list = []
+        for doc in tokens[-self.max_doc:]:
+            sent_list = []
+            for sentence in doc[:self.max_sent]:
+                word_list = []
+                for word in sentence[:self.max_word]:
+                    word_list.append(Token(word))
+                sent_list.append(TextField(word_list, self.token_indexers))
+            doc_list.append(ListField(sent_list))
+        return doc_list
+
+    def text_to_instance(self, user_id: int, tokens: List[List[List[str]]],
+                         subreddit: List[str], timestamp: List[int],
+                         label: Optional[str] = None) -> Instance:
+        user_field = self.tokens_to_user_field(tokens)
+        for post_field, _subreddit, _timestamp in zip(user_field, subreddit[-self.max_doc:], timestamp[-self.max_doc:]):
+            fields = {"tokens": post_field}
+
+            if label:
+                label_field = LabelField(label)
+                fields["label"] = label_field
+
+            fields["meta"] = MetadataField({"user_id": user_id,
+                                            "subreddit": _subreddit,
+                                            "timestamp": _timestamp})
+
+            yield Instance(fields)
+
+    def _read(self, file_path: str) -> Iterator[Instance]:
+        with open(file_path) as f:
+            for line in f:
+                user = json.loads(line.strip())
+
+                for post_instance in self.text_to_instance(user["user_id"],
+                                                           user['tokens'], user["subreddit"],
+                                                           user["timestamp"], user["label"]):
+                    yield post_instance
+
+
 @DatasetReader.register('user_clpsych_time_reader')
 class UserCLPsychTimeDatasetReader(UserCLPsychDatasetReader):
     """
@@ -284,6 +342,73 @@ class UserCLPsychPostTimeDatasetReader(UserCLPsychDatasetReader):
                                             user['tokens'], user["subreddit"],
                                             user["post_id"], user["support"],
                                             user["timestamp"], user["label"])
+
+
+@DatasetReader.register('post_clpsych_post_time_reader')
+class PostCLPsychPostTimeDatasetReader(UserCLPsychDatasetReader):
+    """
+    For pre-sentenized, pre-tokenized json-line SuicideWatch Dataset
+    """
+
+    def tokens_to_user_field(self, tokens) -> ListField:
+        doc_list = []
+        for doc in tokens[-self.max_doc:]:
+            sent_list = []
+            for sentence in doc[:self.max_sent]:
+                word_list = []
+                for word in sentence[:self.max_word]:
+                    word_list.append(Token(word))
+                sent_list.append(TextField(word_list, self.token_indexers))
+            doc_list.append(ListField(sent_list))
+        return doc_list
+
+    def doc_word_counts(self, tokens: List[List[List[str]]]):
+        doc_word_count_list = []
+        for doc in tokens[-self.max_doc:]:
+            totol_word_count = 0
+            for sentence in doc:
+                totol_word_count += len(sentence)
+            doc_word_count_list.append(totol_word_count)
+
+        return doc_word_count_list
+
+    def text_to_instance(self, user_id: int, tokens: List[List[List[str]]],
+                         subreddit: List[str], post_id: List[str],
+                         support: List[List[Any]], timestamp: List[int],
+                         label: Optional[str] = None) -> Instance:
+        user_field = self.tokens_to_user_field(tokens)
+        doc_word_count_list = self.doc_word_counts(tokens)
+        for doc_index, (post_field, doc_word_count) in enumerate(zip(user_field, doc_word_count_list)):
+            fields = {"tokens": post_field}
+            fields["doc_word_counts"] = MetadataField(doc_word_count)
+            fields["support"] = MetadataField(support[-self.max_doc + doc_index])
+
+            if label:
+                label_field = LabelField(label)
+                fields["label"] = label_field
+                raw_meta_field = MetadataField(label)
+                fields["raw_label"] = raw_meta_field
+
+            fields["meta"] = MetadataField({"user_id": user_id,
+                                            "subreddit": subreddit[-self.max_doc + doc_index],
+                                            "post_id": post_id[-self.max_doc + doc_index],
+                                            "timestamp": timestamp[-self.max_doc + doc_index]})
+
+            yield Instance(fields)
+
+    def _read(self, file_path: str) -> Iterator[Instance]:
+        with open(file_path) as f:
+            for line in f:
+                user = json.loads(line.strip())
+
+                for post_intance in self.text_to_instance(user["user_id"],
+                                                          user['tokens'],
+                                                          user["subreddit"],
+                                                          user["post_id"],
+                                                          user["support"],
+                                                          user["timestamp"],
+                                                          user["label"]):
+                    yield post_intance
 
 
 @DatasetReader.register('user_bert_reader')
